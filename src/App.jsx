75fae747a3075485ad8da78d834a2289cc80df3c
irfanshks97 <irfanshks97@gmail.com>
2024-07-01 17:15:30 +0530

    added some libraries and created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <div>
-        <header>
-          <div className="col-6">office</div>
-          <div className="col-6">contact us</div>
-        </header>
-      </div>
+      <Header />;
     </>
   );
 }
