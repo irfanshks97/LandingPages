6f66ce51ece097bd4b311ab80a1e49639c736185
shkirfan67 <shkirfan67@gmail.com>
2024-07-01 12:38:07 +0530

    created landing page react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>
+          <div className="col-6">office</div>
+          <div className="col-6">contact us</div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
